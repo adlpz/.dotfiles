3cd9a4e82154ecc522e756164a453d90dec4b3bd
Adrià López Lozano <adria@prealfa.com>
2014-05-01 18:58:32 +0200

    Fixed intendation of install script

--- install/install.sh
+++ install/install.sh
@@ -10,10 +10,10 @@ echo "~> Linking..."
 
 FILES=$(ls -A $DOTFILES | grep -v install | xargs -n1 -IF echo $(pwd)/../F)
 for f in $FILES
-    do
-            if ln -s $f ~/ 2>/dev/null; then
-                        echo "Linked $(basename $f)"
-                            else
-                                        echo "$(basename $f) already exists in the home directory. Skipping..."
-                                            fi
-                                            done
+do
+    if ln -s $f ~/ 2>/dev/null; then
+        echo "Linked $(basename $f)"
+    else
+        echo "$(basename $f) already exists in the home directory. Skipping..."
+    fi
+done
