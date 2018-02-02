9d5e2d7334fecce68d894e5ebd20744a81502e27
Adria Lopez <adria@prealfa.com>
2018-02-02 17:51:52 +0100

    Improved installer. Added Termite config

--- install/install.sh
+++ install/install.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+
+FMT="%-40s%s\n"
+print_tabbed() {
+    printf $FMT "$1" "$2"
+}
 set -e
 
 cd $(dirname $0)
 
-DOTFILES=../
+DOTFILES=..
 
 echo "~> Linking..."
 
-FILES=$(ls -A $DOTFILES | grep -v 'install\|\.git' | xargs -n1 -IF echo $(pwd)/../F)
+#FILES=$(ls -A $DOTFILES | grep -v 'install\|\.git' | xargs -n1 -IF echo $(pwd)/../F)
+
+WD=$(pwd)
+FILES=$(find $DOTFILES $DOTFILES/.config -maxdepth 1 -not -name ".config" -a -not -name "$DOTFILES" -a -not -name "install" -a -not -name ".git*")
 for f in $FILES
 do
-    if ln -s $f ~/ 2>/dev/null; then
-        echo "Linked $(basename $f)"
+    SRCPATH=$WD/$f
+    DSTPATH=~/$(realpath --relative-to=$DOTFILES $f)
+    COMMAND="ln -sT $SRCPATH $DSTPATH"
+    if [ -L $DSTPATH ]; then
+        print_tabbed "$DSTPATH" "already exists as a symbolic link pointing to $(readlink -f $DSTPATH)"
+    elif [ -f $DSTPATH ]; then
+        print_tabbed $FMT "$DSTPATH" "already exists as a regular file"
+    elif [ -d $DSTPATH ]; then
+        print_tabbed "$DSTPATH" "already exists as a directory."
+    elif [ -e $DSTPATH ]; then
+        print_tabbed "$DSTPATH" "already exists."
     else
-        echo "$(basename $f) already exists in the home directory. Skipping..."
+        echo "$SRCPATH" "Linked to $DSTPATH"
+        $COMMAND
     fi
 done
